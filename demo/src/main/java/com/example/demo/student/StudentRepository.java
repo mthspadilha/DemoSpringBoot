a8bfd839e6dd1d48ed53e00e7b93c433ebb3dc5d
Matheus Padilha <matheuspadilha.m@gmail.com>
2021-06-20 15:01:40 -0300

    package application into jar

--- demo/src/main/java/com/example/demo/student/StudentRepository.java
+++ demo/src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.student;
 
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 //Essa classe abre uma monte de metodos!
 //findAll, findOne...
@@ -9,4 +13,8 @@ import org.springframework.stereotype.Repository;
 //Utilizar no Service, mas aqui habilita, com a interface
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    //SELECT * FROM STUDENT WHERE EMAIL = ?
+    @Query("SELECT s FROM Student s WHERE s.email=?1")
+    Optional<Student> findStudentByEmail(String email);
 }
